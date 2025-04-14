7c682cc284d2a1d556afcc4f6bc1ca64f6c606aa
ChrisRoland <chrisebuberoland@gmail.com>
2025-04-14 13:48:50 +0100

    add: js

---
+++ script.js
@@ -0,0 +1,22 @@
+const ratingButtons = document.querySelectorAll('.rating-option');
+const submitButton = document.querySelector('.submit-button');
+const thankYouState = document.querySelector('.thank-you-state');
+const ratingNumber = document.querySelector('.rating-number');
+
+let selectedRating;
+
+ratingButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    selectedRating = button.textContent;
+    ratingButtons.forEach(btn => btn.classList.remove('selected'));
+    button.classList.add('selected');
+  });
+});
+
+submitButton.addEventListener('click', () => {
+  if (selectedRating) {
+    document.querySelector('.container').classList.add('hidden');
+    thankYouState.classList.remove('hidden');
+    ratingNumber.textContent = selectedRating;
+  }
+});
